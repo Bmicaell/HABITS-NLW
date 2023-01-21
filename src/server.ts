95f1b6ce8e79528be134aff327cb11b436735652
Bruno Micaell <brunom7l@hotmail.com>
2023-01-21 18:51:01 -0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,16 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 import cors from '@fastify/cors'
+import { appRoutes } from "./routes";
 
 
 const app = fastify()
-const prisma = new PrismaClient()
+
 
 
 app.register(cors)
+app.register(appRoutes)
 
 
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany({
-        where:{
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-
-    return habits
-})
 
 app.listen({
     port: 3333,
