dce627547d6102cf4bb7a509d8150f5311b75f67
Bruno Micaell <brunom7l@hotmail.com>
2023-01-25 21:54:14 -0300

    webt

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,21 @@
-import './styles/global.css';
-import { Habit } from "./componets/Habit";
 
-function App() {
+import './styles/global.css'
+import {  Plus } from 'phosphor-react';
+//mport { Habit } from './components/habit'
+
+ export function App() {
+ 
   return (
-    <Habit completed={3}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+      
+     
+    
+      </div>
+    </div>
+
+
   )
 }
 
-export default App
+   
